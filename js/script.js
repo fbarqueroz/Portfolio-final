dfce61e9a603c469ca672abe965e3fb449d104bb
Fabian Barquero <fabianbarquero9@gmail.com>
2021-11-11 11:38:30 -0600

    Agrego cambios en header

--- js/script.js
+++ js/script.js
@@ -43,13 +43,6 @@ $(window).scroll(() => {
     loop: true
   });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-  });
-
   // owl carousel script
   $('.carousel').owlCarousel({
     margin: 20,
@@ -58,18 +51,18 @@ $(window).scroll(() => {
     autoplayTimeOut: 2000,
     autoplayHoverPause: true,
     responsive: {
-      0:{
-          items: 1,
-          nav: false,
+      0: {
+        items: 1,
+        nav: false,
       },
-      600:{
-          items: 2,
-          nav: false,
+      600: {
+        items: 2,
+        nav: false,
       },
-      1000:{
-          items: 3,
-          nav: false,
-      }
-    }
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
   });
 });
