97836b817b39ffa64434291d3c9e4c848e9d7963
Fabian Barquero <fabianbarquero9@gmail.com>
2021-11-23 00:44:37 -0600

    Agrego arreglos de codigo de estilo y responsive en cada seccion

--- js/script.js
+++ js/script.js
@@ -1,46 +1,48 @@
-/* eslint-disable quotes */
+/* eslint-disable no-var */
+/* eslint-disable vars-on-top */
 /* eslint-disable no-undef */
+
 $(document).ready(() => {
-$(window).scroll(() => {
+  $(window).scroll(() => {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
-      $('.navbar').addClass("sticky");
+      $('.navbar').addClass('sticky');
     } else {
-      $('.navbar').removeClass("sticky");
+      $('.navbar').removeClass('sticky');
     }
 
     // scroll-up button show/hide script
     if (this.scrollY > 500) {
-      $('.scroll-up-btn').addClass("show");
+      $('.scroll-up-btn').addClass('show');
     } else {
-      $('.scroll-up-btn').removeClass("show");
+      $('.scroll-up-btn').removeClass('show');
     }
-});
+  });
 
   // slide-up script
   $('.scroll-up-btn').click(() => {
     $('html').animate({ scrollTop: 0 });
     // removing smooth scroll on slide-up button click
-    $('html').css("scrollBehavior", "auto");
+    $('html').css('scrollBehavior', 'auto');
   });
 
   $('.navbar .menu li a').click(() => {
     // applying again smooth scroll on menu items click
-    $('html').css("scrollBehavior", "smooth");
+    $('html').css('scrollBehavior', 'smooth');
   });
 
   // toggle menu/navbar script
   $('.menu-btn').click(() => {
-    $('.navbar .menu').toggleClass("active");
-    $('.menu-btn i').toggleClass("active");
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
   });
 
   // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Designer", "Developer"],
+  var typed = new Typed('.typing', {
+    strings: ['Designer', 'Developer'],
     typeSpeed: 100,
     backSpeed: 60,
-    loop: true
+    loop: true,
   });
 
   // owl carousel script
